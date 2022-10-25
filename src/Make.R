ea7dae1dd59ad50bdaad503a935165ccf5e5d7a1
sdesabbata <s.desabbata@le.ac.uk>
2022-10-25 10:00:18 +0100

    Minor edit

--- src/Make.R
+++ src/Make.R
@@ -102,6 +102,9 @@ rmarkdown::render(paste0(Sys.getenv("RGDS_HOME"), "/src/slides/102-slides-reprod
 cat("\n\n>>> Rendering 103-slides-data-manipulation.Rmd <<<\n\n")
 rmarkdown::render(paste0(Sys.getenv("RGDS_HOME"), "/src/slides/103-slides-data-manipulation.Rmd"), quiet = TRUE, output_dir = paste0(Sys.getenv("RGDS_HOME"), "/docs/slides"))
 
+cat("\n\n>>> Rendering 104-slides-table-operations.Rmd <<<\n\n")
+rmarkdown::render(paste0(Sys.getenv("RGDS_HOME"), "/src/slides/104-slides-table-operations.Rmd"), quiet = TRUE, output_dir = paste0(Sys.getenv("RGDS_HOME"), "/docs/slides"))
+
 # Delete temporary images directory
 unlink(paste0(Sys.getenv("RGDS_HOME"), "/src/slides/images"), recursive=TRUE)
 
