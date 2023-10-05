69396641ace81a6d5eb0361a604bff9716cf84ec
sdesabbata <s.desabbata@le.ac.uk>
2023-10-05 16:49:47 +0100

    Docker r-for-geographic-data-science:4.3.1

--- src/Docker_Build.sh
+++ src/Docker_Build.sh
@@ -1,1 +1,1 @@
-docker build --rm --force-rm -t sdesabbata/r-for-geographic-data-science:latest -t sdesabbata/r-for-geographic-data-science:4.2.2 .
+docker build --rm --force-rm -t sdesabbata/r-for-geographic-data-science:latest -t sdesabbata/r-for-geographic-data-science:4.3.1 .
--- src/Docker_Make.sh
+++ src/Docker_Make.sh
@@ -1,4 +1,4 @@
 REPO_DIR=${PWD}
 echo ""
 echo "Bind mount r-for-geographic-data-science folder from: $REPO_DIR"
-docker run -ti --rm --name r-for-geographic-data-science -u rstudio -v $REPO_DIR:/home/rstudio/r-for-geographic-data-science sdesabbata/r-for-geographic-data-science:4.2.2 Rscript /home/rstudio/r-for-geographic-data-science/src/Make.R /home/rstudio/r-for-geographic-data-science
+docker run -ti --rm --name r-for-geographic-data-science -u rstudio -v $REPO_DIR:/home/rstudio/r-for-geographic-data-science sdesabbata/r-for-geographic-data-science:4.3.1 Rscript /home/rstudio/r-for-geographic-data-science/src/Make.R /home/rstudio/r-for-geographic-data-science
